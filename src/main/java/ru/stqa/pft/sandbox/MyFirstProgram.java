b44d9755b9e96ce7fe1f145805efdbf43dbe61fe
dysik17 <86929085+dysik17@users.noreply.github.com>
2023-03-01 15:04:04 +0300

    Hello World

--- src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,24 +4,6 @@ public class MyFirstProgram {
   public static void main(String[] args) {
     System.out.println("Hello, world!");
 
-    System.out.println(2 + 2);
-    System.out.println(2 * 2);
-    System.out.println(2 / 2);
-    System.out.println(2 - 2);
 
-    System.out.println(1 / 2);
-    System.out.println(1.0 / 2);
-    System.out.println(1 / 2.0);
-    System.out.println(2.0 / 2);
-
-    System.out.println("2" + "2");
-    System.out.println(2 + "2");
-    System.out.println("2" + 2);
-
-    System.out.println(2 + 2 * 2);
-    System.out.println((2 + 2) * 2);
-
-    System.out.println("2 + 2 = " + 2 + 2);
-    System.out.println("2 + 2 = " + (2 + 2));
   }
 }
