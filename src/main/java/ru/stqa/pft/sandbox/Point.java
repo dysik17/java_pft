ff0a90a7a3920cb4445ad4949a20cb9a68be62e0
dysik17 <86929085+dysik17@users.noreply.github.com>
2023-03-01 21:09:30 +0300

    Расстояние между двумя точками

--- src/main/java/ru/stqa/pft/sandbox/Point.java
+++ src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -3,7 +3,7 @@ package ru.stqa.pft.sandbox;
 public class Point {
   public static void main(String[] args){
 
-    Distance d = new Distance(3.0, 5.0, 4.0, 8.0);
+    Distance d = new Distance(2.0, 5.0, 4.0, 8.0);
     System.out.println("Расстояние между двумя точками =  " + d.area());
   }
 }
