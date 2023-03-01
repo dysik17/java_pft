7550da3dd1a1295c071c874e48e1db01440b7d04
dysik17 <86929085+dysik17@users.noreply.github.com>
2023-03-01 21:09:58 +0300

    Расстояние между двумя точками

--- src/main/java/ru/stqa/pft/sandbox/Distance.java
+++ src/main/java/ru/stqa/pft/sandbox/Distance.java
@@ -3,6 +3,7 @@ package ru.stqa.pft.sandbox;
 public class Distance {
   public double p1;
   public double p2;
+
   public Distance(double x1, double y1, double x2, double y2) {
     double x = x2 - x1;
     double y = y2 - y1;
