fab2f7481872ae2bbc80afb9e6830a432b606169
dysik17 <86929085+dysik17@users.noreply.github.com>
2023-03-05 22:36:10 +0300

    Веб-приложение

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactDeletionTests.java
@@ -1,49 +1,15 @@
 package ru.stqa.pft.addressbook;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 public class ContactDeletionTests extends  TestBaseContact {
 
-
   @Test
   public void testContactDeletion() throws Exception {
-    wd.get("http://localhost/addressbook/");
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
     wd.findElement(By.xpath("//img[@alt='Edit']")).click();
     wd.findElement(By.xpath("//div[@id='content']/form[2]/input[2]")).click();
     wd.findElement(By.linkText("home")).click();
     wd.findElement(By.linkText("Logout")).click();
-  }
-
-  @AfterMethod
-  public void tearDown() throws Exception {
-    wd.quit();
-  }
-
-  private boolean isElementPresent(By by) {
-    try {
-      wd.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  private boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
   }
 }
