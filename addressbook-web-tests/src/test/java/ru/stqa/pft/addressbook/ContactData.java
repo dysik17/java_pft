98bbd1b48ad98e236de1dc12c57750351f888464
dysik17 <86929085+dysik17@users.noreply.github.com>
2023-03-06 00:44:15 +0300

    Delete ContactData.java

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
+++
@@ -1,5 +0,0 @@
-package ru.stqa.pft.addressbook;
-
-public record ContactData(String firstName, String lastName, String address, String telephoneHome,
-                          String telephoneMobile, String email) {
-}
