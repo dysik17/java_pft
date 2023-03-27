030b201fdf66c0d094deb5f342831ee7386db8d4
dysik17 <86929085+dysik17@users.noreply.github.com>
2023-03-27 14:58:36 +0300

    web-application (Лекция 6.4 Group/ContactCreation)
    Лекция 6.4. Параметризация тестовых методов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,28 +1,39 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase{
 
-  @Test
-  public void testContactCreation() throws Exception {
+  @DataProvider
+  public Iterator<Object[]> validContacts() {
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[] {new ContactData().withLastName("Popova").withFirstName("Irina").withAddress("Moscow").withTelephoneHome("478965").withTelephoneMobile("891590304").withEmail("IrinaP@mail.ru")});
+    list.add(new Object[] {new ContactData().withLastName("Smirnova").withFirstName("Svetlana").withAddress("Moscow").withTelephoneHome("458963").withTelephoneMobile("891590235").withEmail("SvetlanaS@mail.ru")});
+    list.add(new Object[] {new ContactData().withLastName("Lobova").withFirstName("Milana").withAddress("Moscow").withTelephoneHome("985632").withTelephoneMobile("8915900126").withEmail("MilanaL@mail.ru")});
+    return list.iterator();
+  }
+  @Test(dataProvider = "validContacts")
+  public void testContactCreation(ContactData contact) throws Exception {
     Contacts before = app.contact().all();
     app.contact().creationPage();
     File photo = new File("src/test/resources/stru.png");
-    ContactData contact = new ContactData().withFirstName("Irina").withLastName("Popova").withAddress("Moscow").withTelephoneHome("478965").withTelephoneMobile("891590304").withEmail("IrinaP@mail.ru").withPhoto(photo);
     app.contact().create(contact);
     assertThat(app.contact().count(), equalTo(before.size() + 1));
     Contacts after = app.contact().all();
     assertThat(after, equalTo(
             before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
-  }
+    }
 
   @Test (enabled = false)
   public void testBadContactCreation() throws Exception {
