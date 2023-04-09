e124c22a4aafc61ee03c8a1216cca95951f8079d
dysik17 <86929085+dysik17@users.noreply.github.com>
2023-04-09 23:20:16 +0300

    SOAP

---
+++ soap-sample/src/test/java/ru/stqa/pft/soap/GeoIpServiceTests.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.soap;
+
+import com.lavasoft.GeoIPService;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class GeoIpServiceTests {
+
+  @Test
+  public void testMyIp() {
+    String ipLocation = new GeoIPService().getGeoIPServiceSoap12().getIpLocation("194.58.162.160");
+    assertEquals(ipLocation, "RUS");
+
+  }
+}
