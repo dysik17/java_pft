c595d5d64206b987fcce637c9bcdfdb5d21572f0
dysik17 <86929085+dysik17@users.noreply.github.com>
2023-04-17 20:13:26 +0300

    Задание 3

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PrimeTests.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.sandbox;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PrimeTests {
+
+  @Test
+  public void testPrime(){
+    Assert.assertTrue(Primes.isPrimeFast(Integer.MAX_VALUE));
+  }
+
+  @Test
+  public void testNonPrime(){
+    Assert.assertFalse(Primes.isPrime(Integer.MAX_VALUE - 2));
+  }
+}
