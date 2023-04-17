e066414fe3ee423eab29252f9b54c79deecb857e
dysik17 <86929085+dysik17@users.noreply.github.com>
2023-04-18 00:34:13 +0300

    Задание 16

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddingContactGroupTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddingContactGroupTests.java
@@ -9,6 +9,7 @@ import ru.stqa.pft.addressbook.model.Groups;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import static java.util.stream.Collectors.toList;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -18,49 +19,59 @@ public class AddingContactGroupTests extends TestBase{
   public void ensurePreconditions() {
     if (app.db().contacts().size() == 0) {
       app.goTo().homePage();
-      app.contact().create(new ContactData().withFirstName("Elena").withLastName("Nikolaeva")
-              .withAddress("Moscow").withTelephoneMobile("8914759620")
-              .withEmail("123@mail.ru"));
+      app.contact().creationPage();
+      app.contact().create(new ContactData().withFirstName("Елена").withLastName("Николаевна")
+              .withTelephoneMobile("+7(915)8009090").withEmail("privet@mail.ru"));
       app.goTo().homePage();
     }
   }
+
   @Test
-  public void testContactCreation() throws Exception {
+  public void testAddContactToGroup() {
     ContactData contact = app.db().contacts().iterator().next();
     Groups beforeAdd = contact.getGroups(); //контакты в группах до добавления
 
     Groups groups = app.db().groups(); //группы в БД
     GroupData group = groups.iterator().next();
 
-    //контакт не состоит в группе вообще или не стоит в группе, в которую хотим добавить
-    if (contact.getGroups().size() == 0 || !contact.getGroups().contains(group)) {
+    //контак не состоит в группе вообще или не состоит в группе, в которую хотим добавить
+    if(contact.getGroups().size() == 0 || !contact.getGroups().contains(group)) {
       app.goTo().homePage();
       app.contact().addToGroup(contact, group);
       beforeAdd.add(group);
     } else {
       //если контакт состоит в группе, в которую хотим добавить
-      //есть свободная группа, чтобы добавить
+      //есть свободная группа, чтоб добавить
       if (contact.getGroups().size() < groups.size()) {
-        for (GroupData g : groups) {
-          if (!contact.getGroups().contains(g)) {
-            app.contact().addToGroup(contact, g);
-            beforeAdd.add(group);
-            break;
-          }
-        }
-        //нет свободной группы
-      } else {
+        addContactToFreeGroup(contact, beforeAdd, groups, group);
+      } else { //нет свободной группы и ее нужно создать
         Date date = new Date();
         SimpleDateFormat formatDate = new SimpleDateFormat("dd-MM-yyyy-hh-mm-ss");
-        GroupData newGroup = new GroupData().withName("Группа" + formatDate.format(date))
-                .withFooter("footer").withHeader("header");
+        GroupData newGroup = new GroupData().withName("Группа" + formatDate.format(date)).
+                withFooter("footer").withHeader("header");
+        createGroup(newGroup);
+        app.contact().addToGroup(contact, newGroup);
         beforeAdd.add(newGroup);
       }
     }
 
     Groups afterAdd = app.db().contacts().iterator().next().getGroups();
+    System.out.println(afterAdd);
+    System.out.println(beforeAdd);
+    System.out.println(beforeAdd.withAdded(group));
     assertThat(afterAdd.size(), equalTo(beforeAdd.size()));
-    assertThat(afterAdd, equalTo(beforeAdd.withAdded(group)));
+    assertThat(afterAdd.stream().map(GroupData::getName).collect(toList()),
+            equalTo(beforeAdd.withAdded(group).stream().map(GroupData::getName).collect(toList())));
+  }
+
+  private static void addContactToFreeGroup(ContactData contact, Groups beforeAdd, Groups groups, GroupData group) {
+    for (GroupData g : groups) {
+      if (!contact.getGroups().contains(g)) {
+        app.contact().addToGroup(contact, g);
+        beforeAdd.add(group);
+        break;
+      }
+    }
   }
 
   private void createGroup(GroupData group) {
